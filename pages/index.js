189b8d835870a08ade2709ad4818fa0516a2ae41
Max Kim Stagsted <max@max-it.dk>
2021-05-13 11:36:17 +0200

    no next/image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+// import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 import { Game } from '../components/game'
 
